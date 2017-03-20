d341be21a94b02e8f372c0a2f06afda06b2f8844
gautamkrishnar <r.gautamkrishna@gmail.com>
2017-03-21 01:14:34 +0530

    added bucky commands

--- main.py
+++ main.py
@@ -13,6 +13,61 @@ def voice (str):
     engine.runAndWait()
 
 def reply(str):
+    if (str=="what is the time"):
+        voice(time.strftime('%X %x %Z'))
+    if (str=="what time is it"):
+        voice(time.strftime('%X %x %Z'))
+    if (str=="tell me the time"):
+        voice(time.strftime('%X %x %Z'))
+    if (str=="who are you"):
+        voice("I am Bucky, the new age personal assitant")
+    if (str=="what is your name"):
+        voice("I am Bucky, the new age personal assitant")
+    if (str=="hi"):
+        voice("hello, nice to meet you")
+    if (str=="who created you"):
+        voice("i was created by those 4 people standing next to you")
+    if (str=="how do you do"):
+        voice("i'm doing fine, thanks how are you ")
+    if (str=="where are you"):
+        voice("I am probably inside someone's computer")
+    if (str=="i love you"):
+        voice("I love you too handsome")
+    if (str=="go to hell"):
+        voice("perhaps i have already been there")
+    if (str=="what do you do"):
+        voice("i can recognize and carry out basic voice commands, and predict your expenses with help of machine learning")
+    if (str=="what can you do"):
+        voice("i can recognize and carry out basic voice commands, and predict your expenses with help of machine learning")
+    if (str=="search *"):
+        voice("okay what do you want to search")
+    if (str=="check notifications"):
+        voice("you have no new notifications")
+    if (str=="play music"):
+        voice("playing music")
+    if (str=="who am i"):
+        voice("i'm sorry, what is your name")
+    if (str=="what is my name"):
+        voice("i'm sorry, what is your name")
+    if (str=="how is the weather"):
+        voice("32 degree celcius with chances of rain")
+    if (str=="set a reminder"):
+        voice("ok, what should i remind you of")
+    if (str=="count to 10"):
+        voice("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+    if (str=="toss a coin"):
+        import random
+        x = random.randint(0,1)
+        if (x==0):
+            print "Head"
+        else:
+            print "Tail"
+    if (str=="im confused"):
+        voice("lets toss a coin")
+    if (str=="i am sad"):
+        voice("hi sad, my name is Bucky ha ha ha")
+    if (str==""):
+        voice("I am Bucky, the new age personal assitant")
     if (str=="time"):
         voice(time.strftime('%X %x %Z'))
     if (str=="who are you"):
